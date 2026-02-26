71c4740658d06d3d041418d5a04a2bb1a34bce88
Federico Mutti <federico.mutti@trebi.it>
2026-02-26 09:41:43 +0100

    Init Spring Boot project

---
+++ src/main/java/it/docflow/DocflowBeApplication.java
@@ -0,0 +1,13 @@
+package it.docflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocflowBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocflowBeApplication.class, args);
+	}
+
+}
